e614f08a4b7e876fd49a801a96f4e3ccbdf9ede7
Ram Subhash D <d_r_subhash@yahoo.co.in>
2026-05-13 06:05:53 +0000

    Added the code structure

---
+++ include/dynamic_programming/cut_the_rod_to_maximize_profit.h
@@ -0,0 +1,14 @@
+#ifndef CUT_THE_ROD_TO_MAXIMIZE_PROFIT_H
+#define CUT_THE_ROD_TO_MAXIMIZE_PROFIT_H
+namespace dynamic_programming 
+{
+
+class cut_the_rod_to_maximize_profit 
+{
+public:
+    cut_the_rod_to_maximize_profit() = default;
+
+    void execute();
+};
+
+#endif // CUT_THE_ROD_TO_MAXIMIZE_PROFIT_H
